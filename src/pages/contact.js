d3f2a46928abcfe9b4c72480b8121ad4729bcab9
Margus Lehiste <margus@createcore.com>
2021-03-23 17:11:08 +0200

    team calerie pic

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,15 +1,19 @@
 import React from "react"
 import Layout from "../components/layout"
-import wechat from "../images/wechat.jpg"
 import fujiwe from "../images/wechat-fuji.jpg"
-import whatsapp from "../images/whatsapp.jpg"
 import fujiwhat from "../images/whatsapp-fuji.jpg"
+import teamcal from "../images/team-cal.jpg"
 import ContactList from "../components/contact-form"
 
 export default function Contact() {
   return (
     <Layout tab="Contact">
       <ContactList />
+      <div class="grid grid-cols-1 md:grid-cols-1 gap-8">
+        <div className="md:col-span-1 flex flex-wrap justify-center">
+          <img className="h-4/5" src={teamcal} alt="Team Calerie" />
+        </div>
+      </div>
       <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
         <div className="md:col-span-1 flex flex-wrap justify-center">
           <img className="h-96" src={fujiwe} alt="Fuji Wechat" />
@@ -18,7 +22,6 @@ export default function Contact() {
           <img className="h-96" src={fujiwhat} alt="Fuji Whatsapp" />
         </div>
       </div>
-
     </Layout>
   )
 }
