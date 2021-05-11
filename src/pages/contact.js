5e01c2e1ed503e5eb0d2fd0f76d771e9700012c5
Margus Lehiste <margus@createcore.com>
2021-05-11 17:31:58 +0300

    team height fix for iphone safari

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,15 +10,15 @@ export default function Contact() {
     <Layout tab="Contact">
       <ContactList />
       <div class="grid grid-cols-1 md:grid-cols-1 gap-8">
-        <div className="md:col-span-1 flex flex-wrap justify-center mt-4">
-          <img className="h-4/5" src={teamcal} alt="Team Calerie" />
+        <div className="md:col-span-1 flex flex-wrap justify-center items-center mt-4">
+          <img className="" src={teamcal} alt="Team Calerie" />
         </div>
       </div>
       <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
-        <div className="md:col-span-1 flex flex-wrap justify-center">
+        <div className="md:col-span-1 flex flex-wrap justify-center items-center">
           <img className="h-96" src={fujiwe} alt="Fuji Wechat" />
         </div>
-        <div className="md:col-span-1 flex flex-wrap justify-center">
+        <div className="md:col-span-1 flex flex-wrap justify-center items-center">
           <img className="h-96" src={fujiwhat} alt="Fuji Whatsapp" />
         </div>
       </div>
